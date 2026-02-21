4497e5e55153be9fbe73e59520c103bc90f01b87
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-02-21 20:17:05 +0530

    python5.py

---
+++ python5.py
@@ -0,0 +1,55 @@
+# # using next line 
+str1="this is a string.\nwe are creating it in python."
+print(str1)
+
+# #using tab
+str1="this is a string.\twe are creating it in python."
+print(str1)
+
+# #Concatenation
+str1="vaibhavi"
+str2="shanbhag"
+print(str1+str2)
+
+# #example
+str1="Raksha"
+str2="shanbhag"
+final_str=str1+str2
+print(final_str)
+
+#length of str
+str1="vaibhavi"
+len1=len(str1)
+print(len1)
+
+str2="shanbhag"
+len2=len(str2)
+print(len2)
+
+final_str=str1+""+str2
+print(final_str)
+print(len(final_str))
+
+#INDEXING
+str="vaibhavi shanbhag"
+ch=str[0]
+print(ch)
+
+str="vaibhavi"
+ch=str[4]
+print(ch)
+
+str1="vaibhavi"
+str2="Radhakrishna"
+str3="shanbhag"
+ch1=str1[3]
+ch2=str2[4]
+ch3=str3[2]
+print(ch1)
+print(ch2)
+print(ch3)
+print(len(str1))
+print(len(str2))
+print(len(str3))
+
+
