691a6a521c27dd6a52f49cf71078c0fc4f227209
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-02-22 21:36:52 +0530

    Day6.py

---
+++ Day6.py
@@ -0,0 +1,37 @@
+#slicing
+str="vaibhavi"
+print(str[1:4])
+print(str[0:5])
+
+#negative index
+str="apple"
+print(str[-3:-1])
+
+#string Functions
+str="I am studying python from ApnaCollege"
+print(str.endswith("ege"))
+
+#Capitalize
+str="i am vaibhavi"
+print(str.capitalize())
+
+#replace
+str="I am studying python"
+print(str.replace("o","a"))
+print(str.replace("p","n"))
+
+#find
+str="my age is 20.I am from kumta"
+print(str.find("I"))
+
+#count
+str="I am doing work"
+print(str.count("o"))
+
+#practise question 1
+name=str(input("enter fisrst name:"))
+print(len(name))
+
+#practise question 2
+str="vaibhavi@123"
+print(str.count("@"))
