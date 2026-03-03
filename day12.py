aee60c02b198f0bbd9b34a8b18ab04254c0d6bee
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-03-03 21:40:08 +0530

    day12.py

---
+++ day12.py
@@ -0,0 +1,47 @@
+#Dictionary
+info={
+    "key":"value",
+    "name":"vaibhavi",
+    "learning":"coding"
+}
+print(info)
+
+info2={
+    "name":"vaibhavi",
+    "branch":"CSE",
+    "age":19,
+    "cgpa":"9.8",
+    "city":"kumta"
+}
+print(info2)
+
+info3={
+    "name":"vaibhavi",
+    "subjects":["python","c","java"],
+    "topics":("dict","set"),
+    "age":"19",
+    "is_adult":True,
+    "marks":"94"
+}
+print(info3)
+
+print(type(info3))
+
+print(info3["name"])
+print(info3["subjects"])
+print(info3["topics"])
+print(info3["age"])
+
+info3["name"]="shraddha kapoor"
+print(info3)
+
+info3["age"]="21"
+print(info3)
+
+info3["marks"]="90"
+print(info3)
+
+
+
+
+
